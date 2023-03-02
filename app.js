24c9b679777b2f831602f9d0d3d8d73d241dd9f7
vsvikashsingh <113615996+vsvikashsingh@users.noreply.github.com>
2023-03-02 23:28:00 +0530

    tv maze api
    movie search based on config

---
+++ app.js
@@ -0,0 +1,21 @@
+const makeImages = (shows) =>{
+    for (let result of shows){
+        if(result.show.image){
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
+
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+    console.log('submitted');
+    const searchTerm = form.elements.query.value ;
+    const config = {params : { q: searchTerm } }
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data);
+    form.elements.query.value= ''
+});
+
